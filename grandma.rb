50d2d297d4e143115c2e0f98993319f885a5fedd
sunny-tier-6236 <taycurrie@gmail.com>
2020-03-25 16:13:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,3 +12,16 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
+ = "cuddly"
+ 
+if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+elsif dog == "playful"
+  puts "Playing tug-of-war."
+elsif dog == "cuddly"
+  puts "Snuggling."
+else
+  puts "Reading newspaper."
+end
